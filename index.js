fc7298367c1be2619a1180f4cfe4c89dda6cffa7
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2021-07-29 16:05:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,31 +17,5 @@ client.on('ready', () => {
 client.user.setActivity("chatbot by cwkhan | YouTube.com/cwkhan")
   console.log(`Logged in as ${client.user.tag}!`);
 });
-// chatbot event
-client.on("message", async message => {
-  const cchann = db.get(`cwkchatbotch_${message.guild.id}`);
-  if (cchann === null) return;
-  if (!cchann) return;
-  const sender = client.channels.cache.get(cchann);
-  if (message.channel.name == sender.name) {
-    if (message.author.bot) return;
-    message.content = message.content
-      .replace(/@(everyone)/gi, "everyone")
-      .replace(/@(here)/gi, "here");
-    message.channel.stopTyping();
-    message.channel.startTyping();
-    fetch(
-      `https://api.monkedev.com/fun/chat?msg=${encodeURIComponent(
-        message.content
-      )}&botname=${client.user.username}&ownername=cwkhan`
-    ) 
-      .then(res => res.json())
-      .then(data => {
-        message.lineReply(data.message);
-      });
-    message.channel.stopTyping();
-  }
-});
-
 // login into bot
 client.login(token)
