d856470d9e39443befb0c8dde0ddb1ac6f1a8f53
Cwkhan <65243374+Khanmanan@users.noreply.github.com>
2021-07-30 20:20:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,5 +17,34 @@ client.on('ready', () => {
 client.user.setActivity("chatbot by cwkhan | YouTube.com/cwkhan")
   console.log(`Logged in as ${client.user.tag}!`);
 });
+// Chatbot event
+client.on("message", async message => {
+  const cchann = db.get(`cwkchatbotch_${message.guild.id}`);
+  if (cchann === null) return;
+  if (!cchann) return;
+  const sender = client.channels.cache.get(cchann);
+  if (message.channel.name == sender.name) {
+    if (message.author.bot) return;
+    message.content = message.content
+      .replace(/@(everyone)/gi, "everyone")
+      .replace(/@(here)/gi, "here");
+    message.channel.stopTyping();
+    message.channel.startTyping();
+    fetch(`https://api.snowflake107.repl.co/api/chatbot?message=${encodeURIComponent(message.content)}&name=automodbot`, {
+      headers: {
+        "Authorization": ""
+      }
+    })
+      
+      .then(res => res.json())
+      .then(data => {
+        message.lineReply(data.message);
+      });
+    message.channel.stopTyping();
+  }
+});
+  }
+});
+
 // login into bot
 client.login(token)
